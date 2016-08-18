a032c3148ef37d3f956147956b9ed8ae522c1c66
hishamco <hishamco_2007@yahoo.com>
2016-08-18 04:32:32 +0300

    Add copyright

--- src/Microsoft.AspNetCore.Mvc.RazorPages/Infrastructure/PageActionDescriptor.cs
+++ src/Microsoft.AspNetCore.Mvc.RazorPages/Infrastructure/PageActionDescriptor.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc.Abstractions;
 
 namespace Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure
 {
